83288338edf14db934c35571ed4af9799a12781e
TheEyeOfAr3s <eherzig@protonmail.com>
2022-08-28 06:12:12 +0200

    Formating + v1.1.0

--- src/mod.ts
+++ src/mod.ts
@@ -4,12 +4,17 @@ import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
 
 class Mod implements IPostDBLoadMod
 {
+    private cfg = require("../config.json")
     postDBLoad(container: DependencyContainer)
     {
         const databaseServer = container.resolve<DatabaseServer>("DatabaseServer"); // Item Database Server
         const items = databaseServer.getTables().templates.items; // Database Server
-        Object.values(items).forEach((item) => {
-            item._props.ExaminedByDefault = true;
+        Object.values(items).forEach((item) => 
+        {
+            if (!(this.cfg.exclude_special || (item._parent != "5c99f98d86f7745c314214b3" && item._parent != "5c164d2286f774194c5e69fa")))
+            {
+                item._props.ExaminedByDefault = true;
+            }
         })    
     }
 }
